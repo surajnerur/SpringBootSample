c80b74a3b2fcd7058e75420f27ef6a53cacff8dc
Suraj Nerurkar <suraj.nerurkar@tieto.com>
2019-04-09 18:15:41 +0530

    updated by Suraj

---
+++ samplespringbootcreatedwithspringinitializr/src/main/java/com/samplespringboot/springinitializr/SamplespringbootcreatedwithspringinitializrApplication.java
@@ -0,0 +1,13 @@
+package com.samplespringboot.springinitializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplespringbootcreatedwithspringinitializrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplespringbootcreatedwithspringinitializrApplication.class, args);
+	}
+
+}
---
+++ samplespringbootcreatedwithspringinitializr/src/test/java/com/samplespringboot/springinitializr/SamplespringbootcreatedwithspringinitializrApplicationTests.java
@@ -0,0 +1,16 @@
+package com.samplespringboot.springinitializr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SamplespringbootcreatedwithspringinitializrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
