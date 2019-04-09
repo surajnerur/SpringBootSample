7b2be19316fc79a1749c8e636998832b861a13aa
Suraj Nerurkar <suraj.nerurkar@tieto.com>
2019-04-09 17:55:32 +0530

    updated by Suraj

---
+++ src/main/java/com/samplespringboot/topic/TopicService.java
@@ -0,0 +1,40 @@
+package com.samplespringboot.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+
+	private List<Topic> topics = new ArrayList<Topic>(Arrays.asList(new Topic("spring","springframework","spring framewrok desc"),
+			new Topic("corejava","core java","core java desc"))); 
+	
+	public List<Topic> getAllTopics() {
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+	
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+
+	public void updateTopic(Topic topic, String id) {
+		for(int i=0; i<topics.size();i++) {
+			Topic t = topics.get(i);
+			if(t.getId().equals(id)) {
+				topics.set(i, topic);
+				return;
+			}
+		}
+	}
+
+	public void deleteTopic(String id) {
+		topics.removeIf(t -> t.getId().equals(id));
+	}
+}
